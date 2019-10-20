4fa57ce380d7dc8da48d9d224894312c51388095
DenysV <denisevpm@gmail.com>
2019-10-20 17:20:18 -0500

    First validation

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 // Write your JavaScript code here!
+<script>
+        window.addEventListener("load", function() {
+            document.querySelector("form").addEventListener("submit", function(event) {
+                if(document.querySelector("input [name=pilotName]".value==="") {
+                    alert("All fields are required");
+                    event.preventDefault();
 
+                });
+
+            });
+
+        });
+    
+    
+    
+    
+    </script>
 /* This block of code shows how to format the HTML once you fetch some planetary JSON!
 <h2>Mission Destination</h2>
 <ol>
